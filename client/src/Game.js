304b11ae8a292607ced450016ea77a64f5073f09
Efe <48628713+Raader@users.noreply.github.com>
2020-12-18 21:45:27 +0300

    oops

--- client/src/Game.js
+++ client/src/Game.js
@@ -1,9 +1,8 @@
 import { Container } from "react-bootstrap";
-import {Container,Row,Col } from "react-bootstrap";
 export function Game(){
     return(
         <Container>
-            
+
         </Container>
     )
 }
