5ee754f74288bcd1e7a069cbaa611d34fdccd6a4
Efe <48628713+Raader@users.noreply.github.com>
2020-12-24 20:39:13 +0300

    basic controllers

---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+function register(req, res){
+
+}
+
+function login(req, res){
+
+}
+
+function get(req,res){
+
+}
+
+module.exports.register = register;
+module.exports.login = login;
+module.exports.get = get;
