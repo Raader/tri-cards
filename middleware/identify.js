4c2c859c6a7aa11445bdd29b441abf59df2873d6
Efe <48628713+Raader@users.noreply.github.com>
2021-01-09 13:40:16 +0300

    socket auth middleware

---
+++ middleware/identify.js
@@ -0,0 +1,25 @@
+const userModel = require("../models/User");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+module.exports = (socket,next) => {
+    const token = socket.handshake.auth.token;
+    //check if there is a token
+    console.log("he",!token);
+    if(!token) return next(new Error("no token"));
+    //verify token
+    jwt.verify(token,config.get("secretKey"),function(err,decoded){
+        if(err) return next(new Error("auth failed"));
+        //parse id
+        const id = decoded._id;
+        console.log("decoded",id)
+        //find user info from database
+        userModel.findById(id,(err,doc) => {
+            if(err) return next(err);
+            //add user to socket object
+            socket.user = {name:doc.name,id:doc.id};
+            next()
+        })
+
+    })
+}
