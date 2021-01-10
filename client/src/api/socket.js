39aadb716da4d82871575a29330b586f3e717497
Efe <48628713+Raader@users.noreply.github.com>
2021-01-10 17:15:49 +0300

    heroku

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -2,12 +2,15 @@ import { useEffect } from "react";
 import io from "socket.io-client";
 import { token } from "./join";
 
-const server = "localhost:5000"
+const server = "localhost:5000";
 let socket;
 
 function SocketClient(){
     useEffect(() => {
-        socket = io(server,{auth:{token}});
+        if(process.env.NODE_ENV === "production"){
+            socket = io({auth:{token}});
+        }
+        else socket = io(server,{auth:{token}});
         return(() => socket.disconnect())
     },[])
     return(
--- index.js
+++ index.js
@@ -27,6 +27,17 @@ const io = require("socket.io")(http,{
 io.use(require("./middleware/identify"));
 io.on("connection",(socket) => require("./routes/sockets")(io, socket));
 
+//serve client
+if(process.env.NODE_ENV === "production"){
+    
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req,res) =>{
+      res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+  })
+}
+
 //set port and listen
 const port = process.env.PORT || 5000
 http.listen(port, () => {
