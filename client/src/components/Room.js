956aa41239eb85eb93346409ec7315e826cd9058
Efe <48628713+Raader@users.noreply.github.com>
2021-01-18 17:41:15 +0300

    room style

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
+import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { joinRoom, leaveRoom, subscribeToRoomUsers, unsubscribeFromRoomUsers } from "../api/rooms";
-
+import "../sheets/Room.css"
 export function Room(props){
     const {id} = useParams()
     const [name, setName] = useState("");
@@ -22,9 +23,30 @@ export function Room(props){
         return unsubscribeFromRoomUsers;
     },[id])
     return (
-        <div>
-            <h1>{name}</h1>
-            {users.map(val => <h1>{val.name}</h1>)}
-        </div>
+        <Container id="room-ctn">
+            <Row>
+                <Col className="mx-auto" sm="8" id="room-users-col">
+                    <div id="room-main">
+                    <div id="room-name">
+                        <h1>{name}</h1>
+                    </div>
+                    <div id="room-users">
+                    {users.map((user) => 
+                    <div className="room-user">
+                        <p><i class="fas fa-user"></i> {user.name}</p>
+                    </div>)}
+                    </div>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col sm="8" className="mx-auto" id="room-bottom-col">
+                <div id="room-bottom">
+                <p>Waiting for host to start the game</p>
+                </div>
+                </Col>
+            </Row>
+            
+        </Container>
     )
 }
