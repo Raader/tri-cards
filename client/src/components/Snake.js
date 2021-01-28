574c60d02ad7a4258d4e8024d5f72ade96c90f4a
Efe <48628713+Raader@users.noreply.github.com>
2021-01-28 16:21:26 +0300

    basic p5 setup

--- client/src/components/Snake.js
+++ client/src/components/Snake.js
@@ -1,6 +1,23 @@
+import p5 from "p5";
+import { useEffect, useRef } from "react"
+
 export function Snake(props){
+    const canvas = useRef();
+    let p = useRef();
+    const sketch = (p) => {
+        p.setup = () => {
+            p.createCanvas(500,450)
+        }
+        p.draw = () => {
+            p.background("blue")
+        }
+    }
+
+    useEffect(() => {
+        p.current = new p5(sketch,canvas.current);
+    },[])
     return(
-    <div>
+    <div ref={ref => canvas.current = ref}>
 
     </div>
     )
