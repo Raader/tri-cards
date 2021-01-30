50e2d6510814ee113731d52746ba4f779386a8ab
Efe <48628713+Raader@users.noreply.github.com>
2021-01-30 15:24:08 +0300

    out of physics funtions

--- client/src/game/Physics.js
+++ client/src/game/Physics.js
@@ -20,3 +20,25 @@ export function intersects(area1,area2){
     }
     return false;
 }
+export function pointOutOfArea(point,area) {
+    return point.x < area.x || point.x > area.x + area.width || point.y < area.y || point.y > area.y + area.height
+}
+
+export function areaOutOfArea(area1,area2){
+    const x = area1.x
+    const y = area1.y
+    const width = area1.width;
+    const height = area1.height;
+    const corners = [
+        {x: x,y: y},
+        {x: x + width ,y: y },
+        {x: x + width,y: y + height},
+        {x: x ,y: y + height},
+    ]
+    for(let corner of corners){
+        if(pointOutOfArea(corner,area2)){
+            return true;
+        }
+    }
+    return false;
+}
