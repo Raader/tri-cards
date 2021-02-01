d09523f7c3a87915be91a3e08da77b84a7fed917
Efe <48628713+Raader@users.noreply.github.com>
2021-02-01 12:00:40 +0300

    add physics functions to backend

---
+++ app/game/physics.js
@@ -0,0 +1,49 @@
+function collides(point,area){
+    return area.x <= point.x && point.x <= area.x + area.width && area.y <= point.y && point.y <= area.y + area.height;
+}
+
+function intersects(area1,area2){
+    const x = area1.x
+    const y = area1.y
+    const width = area1.width;
+    const height = area1.height;
+    const corners = [
+        {x: x,y: y},
+        {x: x + width ,y: y },
+        {x: x + width,y: y + height},
+        {x: x ,y: y + height},
+    ]
+    for(let corner of corners){
+        if(collides(corner,area2)){
+            return true;
+        }
+    }
+    return false;
+}
+function pointOutOfArea(point,area) {
+    return point.x < area.x || point.x > area.x + area.width || point.y < area.y || point.y > area.y + area.height
+}
+
+function areaOutOfArea(area1,area2){
+    const x = area1.x
+    const y = area1.y
+    const width = area1.width;
+    const height = area1.height;
+    const corners = [
+        {x: x,y: y},
+        {x: x + width ,y: y },
+        {x: x + width,y: y + height},
+        {x: x ,y: y + height},
+    ]
+    for(let corner of corners){
+        if(pointOutOfArea(corner,area2)){
+            return true;
+        }
+    }
+    return false;
+}
+
+module.exports.collides = collides;
+module.exports.intersects = intersects;
+module.exports.pointOutOfArea = pointOutOfArea;
+module.exports.areaOutOfArea = areaOutOfArea;
