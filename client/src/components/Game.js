1a1bb91ccdc24fb3081655c008ece33f1b3c8f0f
Efe <48628713+Raader@users.noreply.github.com>
2021-02-14 16:24:06 +0300

    rooms column

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -4,14 +4,13 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { useGame } from "../hooks/useGame";
 import "../sheets/Game.css";
-import { CreateRoom } from "./CreateRoom";
 import { ProfileColumn } from "./ProfileColumn";
+import { RoomsColumn } from "./RoomsColumn";
 import { UsersColumn } from "./UsersColumn";
 
 export function Game(props) {
     const game = useGame()
     const history = useHistory();
-    const [show,setShow] = useState(false)
     const [test,setTest] = useState([])
     useEffect(() => {
         const t = []
@@ -22,31 +21,12 @@ export function Game(props) {
     },[])
     return (
         <Container id="game" fluid>
-            <CreateRoom show={show} setShow={setShow} create={
-                (name,g) => game.createRoom(name,g,(err,room) => {
-                            if(err) return console.error(err);
-                            history.push("/room/" + room.id);
-                            })
-                }></CreateRoom>
             <Row>
                 <Col sm="2" id="prof-col">
                 <ProfileColumn user={props.user}></ProfileColumn>
                 </Col>
                 <Col className="no-padding">
-                    <div id="room-list">
-                        <h3>Rooms <span><Button variant="stylish" onClick={() =>
-                        {
-                        setShow(true);
-                        }}>Create Room</Button></span></h3>
-                        
-                        <div id="rooms">
-                        {game.roomList.map(room => !room.started ?
-                        <div className="list-room" onClick={() => history.push("/room/" + room.id)}><p>{room.name}</p></div>
-                        :
-                        <div className="list-room started-room"><p>{room.name}</p></div>  
-                        )}
-                        </div>
-                    </div>
+                    <RoomsColumn game={game}></RoomsColumn>
                 </Col>
                 <Col sm="2" className="no-padding">
                     <UsersColumn userList={game.userList}></UsersColumn>
--- client/src/components/RoomsColumn.js
+++ client/src/components/RoomsColumn.js
@@ -1,21 +1,89 @@
-import { Container,Button, } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Container, Button, Row, Col } from "react-bootstrap";
+import { CreateRoom } from "./CreateRoom";
+import { useHistory } from "react-router-dom";
+import "../sheets/RoomsColumn.css";
 
-export function RoomsColumns() {
+export function RoomsColumn(props) {
+    const history = useHistory();
+    const [show, setShow] = useState(false);
+    const [over,setOver] = useState(false);
+    const [msg,setMsg] = useState("");
+    let typing = false;
+
+    function startTyping(){
+        const message = "Create Room";
+        let i = 0;
+        let ms = ""
+        typing = true;
+        function nextLetter(cb){
+            if(i < message.length && typing){
+                ms += message[i]
+                setMsg(ms)
+                i += 1;
+                setTimeout(nextLetter,15)
+            }
+        }
+        setTimeout(() => {
+            nextLetter()
+        },15)
+    }
     return (
-        <Container fluid>
-            <div id="room-list">
-                <h2>Rooms <span><Button variant="stylish" onClick={() => {
-                    setShow(true);
-                }}>Create Room</Button></span></h2>
+        <Container fluid id="rooms-column">
+            <CreateRoom show={show} setShow={setShow} create={
+                (name, g) => props.game.createRoom(name, g, (err, room) => {
+                    if (err) return console.error(err);
+                    history.push("/room/" + room.id);
+                })
+            }></CreateRoom>
+            <Row>
+                <Col>
+                    <h3>Room Name</h3>
+                </Col>
+                <Col>
+                    <h3>Game Mode</h3>
+                </Col>
+                <Col>
+                    <h3>Players</h3>
+                </Col>
+                <Col>
+                    <h3>Ping</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <div id="room-list">
+                        <div id="rooms">
+                            {props.game.roomList.map(room => !room.started ?
+                                <Container className="list-room" onClick={() => history.push("/room/" + room.id)}>
+                                    <Row>
+                                        <Col>
+                                            <p>{room.name}</p>
+                                        </Col>
+                                        <Col>
+                                            Tank
+                                        </Col>
+                                        <Col>
+                                            5/10
+                                        </Col>
+                                        <Col>
+                                            83ms
+                                        </Col>
+                                    </Row>
 
-                <div id="rooms">
-                    {game.roomList.map(room => !room.started ?
-                        <div className="list-room" onClick={() => history.push("/room/" + room.id)}><p>{room.name}</p></div>
-                        :
-                        <div className="list-room started-room"><p>{room.name}</p></div>
-                    )}
-                </div>
-            </div>
+                                </Container>
+                                :
+                                <div className="list-room started-room"><p>{room.name}</p></div>
+                            )}
+                        </div>
+                        <div className="rooms-footer">
+                            <Button variant="glow" onMouseOver={() => startTyping()} onMouseOut={() => {typing = false;setMsg("")}} onClick={() => {
+                                setShow(true);
+                            }}>{msg ? msg : <i class="fas fa-plus"></i>}</Button>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         </Container>
     )
 }
